460e698ddca30001f8bed8a0b8a98f18c64f2d49
Stilyan Kolarov <stilyankolarov1@gmail.com>
2020-08-04 18:13:48 +0300

    .

--- modules/project/lib/src/gui.dart
+++ modules/project/lib/src/gui.dart
@@ -5,28 +5,30 @@ import 'dart:html';
 
 import 'package:cl/action.dart' as cl_action;
 import 'package:cl/app.dart' as cl_app;
-import 'package:cl/chart.dart' as cl_chart;
 import 'package:cl/base.dart' as cl;
+import 'package:cl/chart.dart' as cl_chart;
 import 'package:cl/forms.dart' as cl_form;
 import 'package:cl/gui.dart' as cl_gui;
 import 'package:cl_base/client.dart' as base;
-import 'package:cl_local/entity.dart';
 import 'package:communicator/client.dart';
-import 'package:local/client.dart' as local;
 import 'package:icon/icon.dart' as icon;
+import 'package:local/client.dart' as local;
 
 import 'entity.dart' as entity;
 import 'path.dart';
 import 'shared.dart';
 
+part 'gui/element/multi_select_user.dart';
+part 'gui/element/multi_select_status.dart';
+part 'gui/element/multi_select_priority.dart';
+part 'gui/element/select_task_priority.dart';
+part 'gui/element/select_task_status.dart';
+part 'gui/element/select_user.dart';
+part 'gui/element/task_status_cell.dart';
 part 'gui/gadgets/task_card.dart';
 part 'gui/gadgets/task_gadget.dart';
 part 'gui/task.dart';
 part 'gui/task_list.dart';
-part 'gui/element/select_task_status.dart';
-part 'gui/element/select_task_priority.dart';
-part 'gui/element/select_user.dart';
-part 'gui/element/task_status_cell.dart';
 
 abstract class Icon {
   static const String Tasks = cl.Icon.message;
---
+++ modules/project/lib/src/gui/element/multi_select_status.dart
@@ -0,0 +1,46 @@
+part of project.gui;
+
+class SelectMultiTaskStatus extends cl_form.SelectMulti {
+  SelectMultiTaskStatus([first]) : super() {
+    addClass('task-status');
+    if (first != null) addOption(first[0], first[1]);
+    TaskStatus.taskStatus
+        .forEach((option) => addOption(option['k'], option['v']));
+  }
+
+  cl.CLElement addOption(dynamic value, dynamic title,
+      [bool initValue = true]) {
+    final li = super.addOption(value, title, initValue);
+    _setClass(li, value);
+    return li;
+  }
+
+  void setShadowValue() {
+    super.setShadowValue();
+    _setClass(domValue, getValue());
+  }
+
+  void _setClass(cl.CLElement el, key) {
+    el
+      ..removeClass('to-do')
+      ..removeClass('done')
+      ..removeClass('test')
+      ..removeClass('in-progress')
+      ..removeClass('for-discussion')
+      ..removeClass('postponed')
+      ..removeClass('canceled');
+    if (key == TaskStatus.ToDo)
+      el.addClass('to-do');
+    else if (key == TaskStatus.Done)
+      el.addClass('done');
+    else if (key == TaskStatus.Test)
+      el.addClass('test');
+    else if (key == TaskStatus.InProgress)
+      el.addClass('in-progress');
+    else if (key == TaskStatus.ForDiscussion)
+      el.addClass('for-discussion');
+    else if (key == TaskStatus.Canceled)
+      el.addClass('postponed');
+    else if (key == TaskStatus.Postponed) el.addClass('canceled');
+  }
+}
