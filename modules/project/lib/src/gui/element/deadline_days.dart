431599e0bcfbd499d4d0c0639a6f3d55df41a1f3
usein <usein@usein.usein>
2020-08-07 15:50:30 +0300

    .

--- modules/project/lib/src/gui/element/deadline_days.dart
+++ modules/project/lib/src/gui/element/deadline_days.dart
@@ -1,13 +1,18 @@
 part of project.gui;
 
-class DateAndRemainingDays extends local.DateTimeCell {
+class DateAndRemainingDays extends cl_form.RowDataCell<List> {
   DateAndRemainingDays(cl_form.GridColumn column, row, cell, object)
       : super(column, row, cell, object);
 
+  local.Date deadLine;
+
   void render() {
-    print(object);
+    final dateCreated = DateTime.parse(object[0]);
+    final deadLineDate = DateTime.parse(object[1]);
+    deadLine = new local.Date(deadLineDate);
+    final days = deadLineDate.difference(dateCreated).inDays;
     cell
       ..style.whiteSpace = 'nowrap'
-      ..text = object == null ? '' : object.getWithTime() ;
+      ..text = object == null ? '' : '${deadLine.getWithTime()} ($days)';
   }
 }
