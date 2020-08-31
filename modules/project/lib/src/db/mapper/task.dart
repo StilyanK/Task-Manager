2e1fe52e49b2ec267a492aa4d1e50f243e54350c
usein <usein@usein.usein>
2020-08-31 15:09:40 +0300

    .

--- modules/project/lib/src/db/mapper/task.dart
+++ modules/project/lib/src/db/mapper/task.dart
@@ -13,17 +13,18 @@ class TaskMapper extends Mapper<Task, TaskCollection, App> {
 
   Future<TaskCollection> findByAllToDo(int user) => loadC(selectBuilder()
     ..where('${entity.$Task.status} != @p1')
+    ..andWhere('${entity.$Task.status} != @p4')
     ..andWhere('${entity.$Task.assigned_to} = @p2')
     ..andWhere('${entity.$Task.is_deleted} = @p3')
     ..setParameter('p1', TaskStatus.Done)
+    ..setParameter('p4', TaskStatus.Canceled)
     ..setParameter('p2', user)
     ..setParameter('p3', false));
 
-  Future<TaskCollection> findAllChildTasks(int task_id) =>
-      loadC(selectBuilder()
-        ..where('${entity.$Task.parent_task} = @p1')
-        ..setParameter('p1', task_id)
-        ..addOrderBy('${entity.$Task.task_id}', 'ASC'));
+  Future<TaskCollection> findAllChildTasks(int task_id) => loadC(selectBuilder()
+    ..where('${entity.$Task.parent_task} = @p1')
+    ..setParameter('p1', task_id)
+    ..addOrderBy('${entity.$Task.task_id}', 'ASC'));
 
   Future<bool> delete(Task object) async {
     object.is_deleted = true;
