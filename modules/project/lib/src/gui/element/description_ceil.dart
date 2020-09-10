ffa4c580195dcded53e5b4257c7255a4738ca53d
Petar Sabev <pisabev@gmail.com>
2020-09-10 03:20:49 +0300

    .

--- modules/project/lib/src/gui/element/description_ceil.dart
+++ modules/project/lib/src/gui/element/description_ceil.dart
@@ -14,10 +14,12 @@ class DescriptionCeil extends cl_form.RowDataCell<List> {
     cell..append(sp)..append(sp2);
     cell.style.maxWidth = '300px';
     if (object != null && object.isNotEmpty) {
-      new cl_app.BubbleVisualizer(
-          new cl.CLElement(cell),
+      final c = new cl.CLElement(cell);
+      final d = new cl_app.BubbleVisualizer(
+          c,
           () => new DivElement()
             ..setInnerHtml('<b>${object.first}</b><div>${object.last}</div>'));
+      c.removeAction('mouseout');
     }
   }
 }
